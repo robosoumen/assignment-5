2f21ee7ba6f6422a363732c73cebacf970c3a8ca
soumen bera <soumenbera211@gmail.com>
2025-08-28 23:52:50 +0530

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// for call button adding event listener
-document.getElementById('btn-call').addEventListener('click',function(){
-    console.log('call button clicked')
+// heart function
+document.getElementById('card-heart').addEventListener('click',function(){
+    const heartValue = parseInt(document.getElementById('banner-heart').innerText)
+    const newHeartvalue = heartValue + 1
+    document.getElementById('banner-heart').innerText = newHeartvalue
 })
+
+
+
+
+// for call button adding event listener
+// document.getElementById('btn-call').addEventListener('click',function(){
+//     console.log('call button clicked')
+// })
